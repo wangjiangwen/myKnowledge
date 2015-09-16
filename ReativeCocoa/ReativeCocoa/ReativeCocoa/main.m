9fd3eb94f97a96e0caef562583275c063d0d1a55
wangjiangwen <1058881673@qq.com>
2015-09-16 23:36:36 +0800

    ReativeCocoa练习

---
+++ ReativeCocoa/ReativeCocoa/ReativeCocoa/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ReativeCocoa
+//
+//  Created by wangjiangjiao on 15/9/13.
+//  Copyright (c) 2015年 wangjiangjiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
